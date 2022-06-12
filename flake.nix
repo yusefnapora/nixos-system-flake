e60b8966dab378189bdf0b080a1aaf50479a7dd8
Yusef Napora <yusef@napora.org>
2022-06-12 22:02:47 +0000

    update nixpkgs to 22.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = { 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11"; 
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05"; 
     home-manager = { 
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
