dbfffe684771d2832797309660deee43d4a2eb66
Yusef Napora <yusef@napora.org>
2022-06-13 04:49:48 +0000

    get rid of broken xclip thing

--- system/programs/sway.nix
+++ system/programs/sway.nix
@@ -6,43 +6,10 @@ let cfg = config.yusef.sway;
 in {
   options.yusef.sway = {
     enable = mkEnableOption "sway";
-    i3status = mkEnableOption "use i3status?";
-    output = mkOption {
-      type = types.attrsOf (types.attrsOf types.str);
-      default = { };
-      example = { "HDMI-A-2" = { bg = "~/path/to/background.png fill"; }; };
-      description = ''
-        An attribute set that defines output modules. See man sway_output for options.
-      '';
-    };
   };
 
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [ wdisplays ];
     programs.sway.enable = true;
-
-    nixpkgs.overlays = [
-      (self: super: {
-        wl-clipboard-x11 = super.stdenv.mkDerivation rec {
-          pname = "wl-clipboard-x11";
-          version = "5";
-
-          src = super.fetchFromGitHub {
-            owner = "brunelli";
-            repo = "wl-clipboard-x11";
-            rev = "v${version}";
-            sha256 = "1y7jv7rps0sdzmm859wn2l8q4pg2x35smcrm7mbfxn5vrga0bslb";
-          };
-
-          dontBuild = true;
-          dontConfigure = true;
-          propagatedBuildInputs = [ super.wl-clipboard ];
-          makeFlags = [ "PREFIX=$(out)" ];
-        };
-
-        xsel = self.wl-clipboard-x11;
-        xclip = self.wl-clipboard-x11;
-      })
-    ];
   };
 }
