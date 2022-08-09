184382463d6f57f6aa4b83a01a54c122f1f22f7d
Yusef Napora <yusef@napora.org>
2022-08-09 14:30:29 -0400

    tweak i3 config (border colors)

--- home-manager/i3.nix
+++ home-manager/i3.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, nixosConfig, ...}:
 let
-  inherit (lib) mkIf mkOptionDefault;
+  inherit (lib) mkIf mkOptionDefault mkForce;
 
   cfg = nixosConfig.yusef.i3;
   mod = "Mod4";
@@ -12,58 +12,66 @@ in
 
       xsession.enable = true;
       
-        xsession.windowManager.i3 = {
-            enable = true;
-            package = pkgs.i3-gaps;
+      xsession.windowManager.i3 = {
+        enable = true;
+        package = pkgs.i3-gaps;
 
-            config = {
-                modifier = mod;
+        config = {
+            modifier = mod;
 
-                gaps = cfg.gaps;
+            gaps = cfg.gaps;
 
-                keybindings = mkOptionDefault {
-                    # terminal
-                    "${mod}+Return" = "exec kitty";
+            keybindings = mkOptionDefault {
+                # terminal
+                "${mod}+Return" = "exec kitty";
 
-                    # rofi
-                    "${mod}+d" = "exec --no-startup-id rofi -show drun";
+                # rofi
+                "${mod}+d" = "exec --no-startup-id rofi -show drun";
 
-                    # alternative to mod+shift+q, since macos insists on eating it
-                    "${mod}+Shift+w" = "kill";
+                # alternative to mod+shift+q, since macos insists on eating it
+                "${mod}+Shift+w" = "kill";
 
-                    # alternative to mod+w for tabs, since t is for tabs
-                    "${mod}+t" = "layout tabbed";
+                # alternative to mod+w for tabs, since t is for tabs
+                "${mod}+t" = "layout tabbed";
 
-                    # vim-style focus / movement
-                    "${mod}+h" = "focus left";
-                    "${mod}+j" = "focus down";
-                    "${mod}+k" = "focus up";
-                    "${mod}+l" = "focus right";
-                    "${mod}+Shift+h" = "move left";
-                    "${mod}+Shift+j" = "move down";
-                    "${mod}+Shift+k" = "move up";
-                    "${mod}+Shift+l" = "move right";
+                # vim-style focus / movement
+                "${mod}+h" = "focus left";
+                "${mod}+j" = "focus down";
+                "${mod}+k" = "focus up";
+                "${mod}+l" = "focus right";
+                "${mod}+Shift+h" = "move left";
+                "${mod}+Shift+j" = "move down";
+                "${mod}+Shift+k" = "move up";
+                "${mod}+Shift+l" = "move right";
 
-                    # split horizontal moves to Mod+b, since Mod+h is repurposed
-                    "${mod}+v" = "split v";
-                    "${mod}+b" = "split h";
-                };
-
-                # polybar is started by home-manager's systemd service
-                bars = [ ];
-
-                startup = cfg.startup ++ [
-                  { command = "feh --bg-scale --zoom fill ${backgroundImage}"; }
-                  { command = "i3-msg 'workspace 1'"; }
-                ];
-
-                floating.modifier = mod;
-
-                floating.criteria = [
-                  { title = ".zoom "; }
-                ];
+                # split horizontal moves to Mod+b, since Mod+h is repurposed
+                "${mod}+v" = "split v";
+                "${mod}+b" = "split h";
             };
+
+            colors.focused = {
+              border = "#00AF91";
+              childBorder = "#007965";
+              background = "#285577";
+              text = "#ffffff";
+              indicator = "#2e9ef4";
+            };
+
+            # polybar is started by home-manager's systemd service
+            bars = [ ];
+
+            startup = cfg.startup ++ [
+              { command = "feh --bg-scale --zoom fill ${backgroundImage}"; }
+              { command = "i3-msg 'workspace 1'"; }
+            ];
+
+            floating.modifier = mod;
+
+            floating.criteria = [
+              { title = ".zoom "; }
+            ];
         };
+    };
        
     # enable picom compositor, so we can have transparency in polybar & other cool stuff
     services.picom = {
