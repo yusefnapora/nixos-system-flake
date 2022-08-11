9493c6e3d07dd88ea465feaf9d2def3ee676f3a1
Yusef Napora <yusef@napora.org>
2022-08-11 15:30:26 -0400

    fix some transparency glitches

--- home-manager/i3.nix
+++ home-manager/i3.nix
@@ -102,7 +102,12 @@ in
       shadow = true;
       shadowOffsets = [ (-7) (-7) ];
       shadowOpacity = 0.7;
-      shadowExclude = [ "window_type *= 'normal' && ! name ~= ''" ];
+      shadowExclude = [ 
+        "window_type *= 'normal' && ! name ~= ''"
+        "_NET_WM_STATE@:32a *= '_NET_WM_STATE_HIDDEN'" # don't draw multiple shadows for tabbed windows
+        "class_g = 'firefox' && argb" # fix odd shadows for some firefox windows
+          "class_i = 'rofi'" # disable shadows for rofi to fix odd corner rendering
+      ];
 
       activeOpacity = 1.0;
 
@@ -110,9 +115,14 @@ in
       # using this instead of inactiveOpacity, since the latter is
       # too distracting when e.g. coding with a web-browser in split screen
       opacityRules = [
-        ''
-        80: class_i = "kitty" && focused != 1
-        ''
+        "80: class_i = 'kitty' && focused != 1"
+      
+        # don't render hidden windows (prevents semi-transparent tabbed windows)
+        "0:_NET_WM_STATE@[0]:32a *= '_NET_WM_STATE_HIDDEN'"
+        "0:_NET_WM_STATE@[1]:32a *= '_NET_WM_STATE_HIDDEN'"
+        "0:_NET_WM_STATE@[2]:32a *= '_NET_WM_STATE_HIDDEN'"
+        "0:_NET_WM_STATE@[3]:32a *= '_NET_WM_STATE_HIDDEN'"
+        "0:_NET_WM_STATE@[4]:32a *= '_NET_WM_STATE_HIDDEN'"
       ];
 
       backend = "xrender";
