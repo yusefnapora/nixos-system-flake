e2eb55af73e8ee62c23a7fbc7e89384efc0a41a8
Yusef Napora <yusef@napora.org>
2023-02-14 13:57:04 -0500

    ssh: setup proxy jump config to ssh into wsl on hex

--- home-manager/ssh.nix
+++ home-manager/ssh.nix
@@ -11,6 +11,21 @@
         hostname = "deneb.usbx.me";
         user = "yusef";
       };
+
+      # WSL on hex via proxy jump config
+      # see https://kleinfelter.com/3-ways-to-ssh-to-a-pc-running-windows-and-wsl2
+      hex-wsl = {
+        hostname = "127.0.0.1";
+        port = 2022;
+        proxyJump = "hex-win";
+        user = "yusef";
+      };
+
+      # Hex (windows host). LAN only
+      hex-win = {
+        hostname = "hex.lan";
+        user = "yusef";
+      };
     };
   };
 }
