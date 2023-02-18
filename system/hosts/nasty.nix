6d7e8e5a6fa222788e5483c926e0c956d742aa8b
Yusef Napora <yusef@napora.org>
2023-02-18 09:56:25 -0500

    nasty: add samba server config

--- system/hosts/nasty.nix
+++ system/hosts/nasty.nix
@@ -26,8 +26,45 @@
   boot.supportedFilesystems = [ "zfs" ];
   # set hostId to first 8 chars of /etc/machine-id
   networking.hostId = "f94f2c6c";
+  # import pools on boot
+  boot.zfs.extraPools = [ "ocean" ];
 
   systemd.targets.hibernate.enable = false;
+
+  # Samba config
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    openFirewall = true;
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = nasty
+      netbios name = nasty
+      security = user
+      hosts allow = 192.168.86. 127.0.0.1 localhost
+      hosts deny = 0.0.0.0/0
+      guest account = nobody
+      map to guest = bad user
+    '';
+    shares = let 
+      common_attrs = {
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "yusef";
+        "force group" = "users";      
+      };
+    in {
+      media = {
+        path = "/ocean/media";
+      } // common_attrs;
+      documents = {
+        path = "/ocean/documents";
+      } // common_attrs;
+    };
+  };
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
