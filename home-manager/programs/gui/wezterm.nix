fbc9a329ad199cf4f1dbfc953c9c2e6512bd69b0
Yusef Napora <yusef@napora.org>
2023-04-03 14:31:44 -0400

    wezterm: use CopyTo and PasteFrom actions

--- home-manager/programs/gui/wezterm.nix
+++ home-manager/programs/gui/wezterm.nix
@@ -93,8 +93,8 @@ in {
               { key = "x", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
 
               { key = "n", mods="SHIFT|CTRL",     action="ToggleFullScreen" },
-              { key = "v",   mods="SHIFT|CTRL",     action="Paste"},
-              { key = "c",   mods="SHIFT|CTRL",     action="Copy"},
+              { key = "v",   mods="SHIFT|CTRL",     action=wezterm.action.PasteFrom 'Clipboard'},
+              { key = "c",   mods="SHIFT|CTRL",     action=wezterm.action.CopyTo 'Clipboard'},
           },
         }
       '';
