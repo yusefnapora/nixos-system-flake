ef1eefbf0b4d85375e421c78da184a02e3260e58
Yusef Napora <yusef@napora.org>
2023-04-16 14:29:14 -0400

    wip: add yabai to darwin config

--- darwin/hosts/macbook.nix
+++ darwin/hosts/macbook.nix
@@ -8,5 +8,22 @@ in
     ../options.nix
   ];
 
+
+  # TODO: pull yabai config into module if we like it
+  services.yabai = {
+    enable = true;
+    config = {
+      focus_follows_mouse = "autoraise";
+      mouse_follows_focus = "off";
+      window_placement    = "second_child";
+      window_opacity      = "off";
+      top_padding         = 10;
+      bottom_padding      = 10;
+      left_padding        = 10;
+      right_padding       = 10;
+      window_gap          = 10;
+    };
+  };
+
   system.stateVersion = 4;
 }
