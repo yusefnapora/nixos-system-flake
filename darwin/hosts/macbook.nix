a9b4494d99cef3eb671f23e3ecb336811d715530
Yusef Napora <yusef@napora.org>
2023-04-27 07:46:18 -0400

    wip yabai config

--- darwin/hosts/macbook.nix
+++ darwin/hosts/macbook.nix
@@ -1,6 +1,7 @@
 { pkgs, lib, inputs, ... }:
 let
   inherit (inputs) nixpkgs;
+  inherit (lib) lists strings;
 in
 {
   imports = [
@@ -10,20 +11,56 @@ in
 
 
   # TODO: pull yabai config into module if we like it
-  services.yabai = {
+  services.yabai = let
+    unmanaged-apps = [
+      "System Settings"
+      "System Preferences"
+      "Zoom"
+      "zoom.us"
+    ];
+    app-rules = lists.forEach unmanaged-apps (app: 
+      "yabai -m rule --add app='${app}' manage=off"
+      );
+    app-rules-str = strings.concatStringsSep "\n" app-rules;
+  in {
     enable = true;
     config = {
-      focus_follows_mouse = "autoraise";
-      mouse_follows_focus = "off";
       window_placement    = "second_child";
       window_opacity      = "off";
-      top_padding         = 10;
-      bottom_padding      = 10;
-      left_padding        = 10;
-      right_padding       = 10;
-      window_gap          = 10;
+      window_shadow       = "float";
     };
+    extraConfig = ''
+      ${app-rules-str}
+    '';
   };
+
+
+  services.skhd = let
+    hyper = "ctrl + cmd + alt";
+    wezterm = "/Applications/WezTerm.app/Contents/MacOS/wezterm"; # bit of nix heresy.. but the verion of wezterm in nixpkgs is a little wonky, so I just run it from a global manual install ATM. TODO: maybe pull from homebrew (via nix)
+    yabai = "${pkgs.yabai}/bin/yabai";
+  in {
+    enable = true;
+    skhdConfig = ''
+    ${hyper} - return : ${wezterm} start
+
+    # focus next space, wrapping around to first space
+    ${hyper} - tab : ${yabai} -m space --focus next || ${yabai} -m space --focus first
+
+    # focus prev space, wrapping back to last space
+    ${hyper} + shift - tab : ${yabai} -m space --focus prev || ${yabai} -m space --focus last
+
+    # ${hyper} - tab : ${yabai} -m window --swap next
+    # ${hyper} + shift - tab : ${yabai} -m window --swap prev
+
+    ${hyper} - h : ${yabai} -m window --swap west
+    ${hyper} - j : ${yabai} -m window --swap south
+    ${hyper} - k : ${yabai} -m window --swap north
+    ${hyper} - l : ${yabai} -m window --swap east
+    '';
+  };
+
+  environment.systemPackages = [ pkgs.skhd ];
 
   system.stateVersion = 4;
 }
